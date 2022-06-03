fdff55dd03b4eadda3910b3b4fdf2132374a3b59
Soumitra <projwebinfo@gmail.com>
2022-06-03 12:56:19 +0530

    Update RestResource.java

--- rest-jersey-multivalued-map/src/main/java/com/roytuts/rest/jersey/multivalued/map/resources/RestResource.java
+++ rest-jersey-multivalued-map/src/main/java/com/roytuts/rest/jersey/multivalued/map/resources/RestResource.java
@@ -1,8 +1,8 @@
 package com.roytuts.rest.jersey.multivalued.map.resources;
 
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MultivaluedMap;
 
 @Path("/resource")
 public class RestResource {
@@ -10,13 +10,13 @@ public class RestResource {
 	@POST
 	@Path("/multivalued")
 	public String postMultivalued(MultivaluedMap<String, String> multivaluedMap) {
-		
+
 		if (multivaluedMap.get("fname") != null && multivaluedMap.get("lname") != null) {
 			String fname = multivaluedMap.get("fname").get(0);
 			String lname = multivaluedMap.get("lname").get(0);
 			return "Your name : " + fname + " " + lname;
 		}
-		
+
 		return "I have not received anything from you";
 	}
 
