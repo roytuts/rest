fc50cd73e2a5d882f056df8841d75284e9830b63
Soumitra <projwebinfo@gmail.com>
2022-06-03 12:55:55 +0530

    Update RestResourceTest.java

--- rest-jersey-multivalued-map/src/test/java/com/roytuts/rest/jersey/multivalued/map/resources/RestResourceTest.java
+++ rest-jersey-multivalued-map/src/test/java/com/roytuts/rest/jersey/multivalued/map/resources/RestResourceTest.java
@@ -2,19 +2,19 @@ package com.roytuts.rest.jersey.multivalued.map.resources;
 
 import java.net.URI;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MultivaluedHashMap;
-import javax.ws.rs.core.MultivaluedMap;
-
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.MultivaluedMap;
 
 public class RestResourceTest {
 
