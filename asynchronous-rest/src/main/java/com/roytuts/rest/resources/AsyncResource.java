b2603cfd369fb8cea7a02f46f799fc5c963c281c
Soumitra <projwebinfo@gmail.com>
2022-06-07 12:44:56 +0530

    Create AsyncResource.java

---
+++ asynchronous-rest/src/main/java/com/roytuts/rest/resources/AsyncResource.java
@@ -0,0 +1,22 @@
+package com.roytuts.rest.rseources;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+@Path("/resource")
+public class AsyncResource {
+	@GET
+	@Path("/simpleAsync")
+	public void asyncGet(@Suspended final AsyncResponse asyncResponse) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				String result = veryExpensiveOperation();
+				asyncResponse.resume(result);
+			}
+			private String veryExpensiveOperation() {
+				return "Very Expensive Operation";
+			}
+		}).start();
+	}
+}
