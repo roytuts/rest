ee69fefa8e596d68848813d3c8337b7d7a47a88b
Soumitra <projwebinfo@gmail.com>
2022-06-21 18:10:58 +0530

    Create AsyncResourceTest_3.x.java

---
+++ asynchronous-rest-completion-callback/src/test/java/com/roytuts/asynchronous/rest/completion/callback/resources/AsyncResourceTest_3.x.java
@@ -0,0 +1,61 @@
+package com.roytuts.asynchronous.rest.completion.callback.resources;
+
+import java.net.URI;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import jakarta.ws.rs.client.AsyncInvoker;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.Response;
+
+public class AsyncResourceTest {
+
+	private HttpServer httpServer;
+	private WebTarget webTarget;
+	private static final URI baseUri = URI.create("http://localhost:9090/rest/");
+
+	@Before
+	public void setup() throws Exception {
+		// create ResourceConfig from Resource class
+		ResourceConfig rc = new ResourceConfig(AsyncResource.class);
+		// create the Grizzly server instance
+		httpServer = GrizzlyHttpServerFactory.createHttpServer(baseUri, rc);
+		// start the server
+		httpServer.start();
+		// configure client with the base URI path
+		Client client = ClientBuilder.newClient();
+		webTarget = client.target(baseUri);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// if you want to stop the server from the input through keyboard then uncomment
+		// below two lines
+		// System.out.println(String.format("Application started.%nHit enter to stop
+		// it..."));
+		// System.in.read();
+		// stop the server
+		httpServer.shutdown();
+	}
+
+	@Test
+	public void testAsyncGetCompletionCallback() throws InterruptedException, ExecutionException {
+		final AsyncInvoker asyncInvoker = webTarget.path("resource/asyncCompCallback").request().async();
+		final Future<Response> responseFuture = asyncInvoker.get();
+		System.out.println("Request is being processed asynchronously.");
+		final Response response = responseFuture.get();
+		// get() waits for the response to be ready
+		System.out.println("Response received : " + response);
+		System.out.println("Response from GET method : " + response.readEntity(String.class));
+	}
+
+}
